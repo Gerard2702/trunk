685c02105da1a5bbea3332c1607a324d81195609
Romers25 <manoloromer25@gmail.com>
2015-06-08 17:09:44 -0600

    Agregado Formulario Construccion
    Frm Construccion Detalle

---
+++ librerias/formularios/frm_detalles_construccion.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <!-- Estilos Bootstrap -->
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.css">
+    <!-- Estilos Personalizados -->
+    <link rel="stylesheet" type="text/css" href="../librerias/css/style.css">
+    <!-- Estilos ccs Font-awesome -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+</head>
+
+<body>
+<br>
+<div class="container">
+	
+
+		<div class="main row">
+		
+		<div class="col-lg-12">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            Detalles de construccion y Tenencia de Animales
+                        </div>
+                        <div class="panel-body">
+                          <div class="col-md-3">
+                            <div class="form-group">
+                                            <label>Tenencia de La Vivienda</label>
+                                                <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Propia
+                                                </label>
+                                            </div>
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Alquilada
+                                                </label>
+                                            </div>
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Colono/Guardian
+                                                </label>
+                                            </div>
+                         </div>
+                         </div>
+
+                          <div class="col-md-3">
+                            <div class="form-group">
+                                            <label>Material Predominante de Piso</label>
+                                               <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Ceramica,Ladrillo
+                                                </label>
+                                            </div>
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Tierra
+                                                </label>
+                                            </div>
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Otro
+                                                </label>
+                                            </div>
+                            </div>
+		                  </div>
+
+                          <div class="col-md-3">
+                            <div class="form-group">
+                                            <label>Material Predominante de Paredes</label>
+                                               <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Ladrillo/Bloque
+                                                </label>
+                                               </div>
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Adobe
+                                                </label>
+                                            </div>
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Bahareque
+                                                </label>
+                                            </div>
+                            </div>
+                          </div>
+
+                          
+
+                                            <div class="col-md-3">
+
+                                             <div class="form-group">
+                                            <label>Material Predominante de Techo</label>
+                                               <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Asbesto/Duralita
+                                                </label>
+                                               </div>
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Metal(Lamina)
+                                                </label>
+                                            </div>
+                                            <div class="radio">
+                                                <label>
+                                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Teja de Barro/Cemento
+                                                </label>
+                                            </div>
+                            </div>
+                                            
+                          </div>
+
+                          <div class="container">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Vulnerabilidad</label>
+                                              <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Deslave
+                                                </label>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Inundaciones
+                                                </label>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Contaminacion por disposicion no adecuada de desechos solidos, quimicos.
+                                                </label>
+                                            </div>
+                            </div>
+                                     </div>
+
+                                        <div class="col-md-3">
+                                            <div class="form-group">
+                                                <label>Vectores</label>
+                                                 
+                                                          <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Zancudos
+                                                </label>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Moscas
+                                                </label>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Chinches Picudas
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Cucarachas
+                                            </div>
+                                              <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" value="">Roedores
+                                            </div>
+
+                                            </div>
+                                        </div>
+
+                                     
+                            
+                            
+                                </div>                          
+                        </div>
+                             
+                        </div>  
+                        </dvi>
+                     
+                          
+    </div>
+</div>
+
+<script src="./bootstrap/js/jquery.js"></script>
+<script src="./bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
